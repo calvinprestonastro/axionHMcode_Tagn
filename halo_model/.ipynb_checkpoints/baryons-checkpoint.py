935fc418494b9004be45748199b5bbb61da0a899
Calvin Preston <cp662@cam.ac.uk>
2024-05-06 22:47:17 +0100

    Tagn progress

--- halo_model/.ipynb_checkpoints/baryons-checkpoint.py
+++ halo_model/.ipynb_checkpoints/baryons-checkpoint.py
@@ -18,7 +18,12 @@ def func_dens_profile_kspace_baryons(M, k, k_sigma, PS_sigma, cosmo_dic, hmcode_
     Wk = func_dens_profile_kspace(M, k, k_sigma, PS_sigma, cosmo_dic, hmcode_dic, Omega_0, Omega_0_sigma, eta_given = False, axion_dic=None)
 
     feedback_params = _get_feedback_parameters(Tagn) # Calling feedback parameters, calibrated on the BAHAMAS simulations, based off of T_agn
-     # Gas content (Eq. 24 with beta=2)
+    print("feedback params", feedback_params)
+    print("feedback_params['f0']",feedback_params['f0'])
+    print("feedback_params['Mb0']",feedback_params['Mb0'])
+    print("M", M)
+    print("ratio M/Mb", M/feedback_params['Mb0'])
+    # Gas content (Eq. 24 with beta=2)
     M=np.array([M])
     #print("shape of M", np.shape(M))
     #print("number of dimensions in M",np.ndim(M))
@@ -29,13 +34,12 @@ def func_dens_profile_kspace_baryons(M, k, k_sigma, PS_sigma, cosmo_dic, hmcode_
     for j in range(0,len(k)):
         for i in range(0, len(M) ):
             if np.ndim(M)>1:
-                Wk[i,j] =  (cosmo_dic['omega_d_0']/cosmo_dic['omega_m_0']+(cosmo_dic['omega_b_0']/cosmo_dic['omega_m_0']-feedback_params['f0'])*(M[0,i]/feedback_params['Mb0'])**2/(1.+(M[i,0]/feedback_params['Mb0'])**2))*Wk[i,j] + feedback_params['f0']*(M[0,i]/func_rho_comp_0(Omega_0)  ) # / (M[0,i]/func_rho_comp_0(Omega_0) )
+                Wk[i,j] = (cosmo_dic['omega_d_0']/cosmo_dic['omega_m_0']+(cosmo_dic['omega_b_0']/cosmo_dic['omega_m_0']-feedback_params['f0'])*(M[0,i]/feedback_params['Mb0'])**2/(1.+(M[i,0]/feedback_params['Mb0'])**2))*Wk[i,j] + feedback_params['f0'] #*(M[0,i]/func_rho_comp_0(Omega_0))**2  # / (M[0,i]/func_rho_comp_0(Omega_0) )
                 
             else:
-                Wk[i,j] =  (cosmo_dic['omega_d_0']/cosmo_dic['omega_m_0']+(cosmo_dic['omega_b_0']/cosmo_dic['omega_m_0']-feedback_params['f0'])*(M[i]/feedback_params['Mb0'])**2/(1.+(M[i]/feedback_params['Mb0'])**2))*Wk[i,j] + feedback_params['f0']*(M[i]/func_rho_comp_0(Omega_0) ) # / (M[i]/func_rho_comp_0(Omega_0) )
+                Wk[i,j] = (cosmo_dic['omega_d_0']/cosmo_dic['omega_m_0']+(cosmo_dic['omega_b_0']/cosmo_dic['omega_m_0']-feedback_params['f0'])*(M[i]/feedback_params['Mb0'])**2/(1.+(M[i]/feedback_params['Mb0'])**2))*Wk[i,j] + feedback_params['f0']#*(M[i]/func_rho_comp_0(Omega_0))**2  # / (M[i]/func_rho_comp_0(Omega_0) )
            
         #Wk += feedback_params['f0']
-    
     print("Wk now with baryons",Wk)
     print("shape of Wk",np.shape(Wk))
     return Wk
